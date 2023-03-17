7972eac805656f31ea5fd7949136bfa17592154c
Jorge Antezana <102924003+jorgevfx@users.noreply.github.com>
2023-03-16 19:18:17 -0500

    project creation

---
+++ src/main/java/com/george/cfopapi/CfopApiApplication.java
@@ -0,0 +1,13 @@
+package com.george.cfopapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfopApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CfopApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/george/cfopapi/CfopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.george.cfopapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CfopApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
